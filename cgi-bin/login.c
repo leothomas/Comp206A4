7fd909f6304227ff01c1155945dd69dc5455ef97
Leo THOMAS <leo.thomas@mail.mcgill.ca>
2015-04-12 15:33:32 -0400

    error 405 with login.c and register.cgi

--- cgi-bin/login.c
+++ cgi-bin/login.c
@@ -16,16 +16,16 @@ int n = atoi(getenv("CONTENT_LENGTH")); */
 //	TEST COMMENT CHECK FOR OVERRIDES
 
 int main(void){
-	char *data = getenv("QUERY_STRING");
-	char username[25], password[25];
+	//char *data = getenv("QUERY_STRING");
+	//char username[25], password[25];
 
-	sscanf(data, "%s %s", &username, &password);
-	printf("Content-Type:text/html\n\n");
+	//sscanf(data, "%s %s", &username, &password);
+	printf("%s%c%c\n","Content-Type:text/html;charset=iso-8859-1",13,10);
 	printf("<html>");
 	
-	//printf("<head>");
-	//printf("<title>Test post please ignore</title>");
-	//printf("</head>");
+	printf("<head>");
+	printf("<title>Test post please ignore</title>");
+	printf("</head>");
 	printf("<body> <h1><center> MontrealBarBook</h1></center></body>");
 	// printf("username: %s, password: %s", username, password);
 	printf("</html>");
